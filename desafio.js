aeb5fa902b20f4d675f3233c2b5f1cde29ef5e66
PauloIgorB <pauloigor462@gmail.com>
2024-10-31 10:46:59 -0300

    desafio de logica concluido

--- desafio.js
+++ desafio.js
@@ -1,30 +1,30 @@
-let nome = "paulo"
+let nome = "Paulo"
 let xp = 0
-let rank = ""
+let rank
 
-if (xp <= 1000);
+if (xp <= 1000){
     rank = "ferro"
-
-if (xp > 1000) (xp <= 2000);
+}
+if (xp > 1000 && xp <= 2000){
     rank = "bronze"
-
-if (xp > 2000) (xp <= 5000);
+}
+if (xp > 2000 && xp <= 5000){
     rank = "prata"
-
-if (xp > 5000) (xp <= 7000);
+}
+if (xp > 5000 && xp <= 7000){
     rank = "ouro"
-
-if (xp > 7000) (xp <= 8000);
+}
+if (xp > 7000 && xp <= 8000){
     rank = "platina"
-
-if (xp > 8000) (xp <= 9000);
+}
+if (xp > 8000 && xp <= 9000){
     rank = "ascendente"
-
-if (xp > 9000) (xp <= 10000);
+}
+if (xp > 9000 && xp <= 10000){
     rank = "imortal"
-
-if (xp > 10000);
+}
+if (xp > 10000){
     rank = "radiante"
-
+}
 
 console.log("O herói, " + nome + ", está no rank " + rank)
